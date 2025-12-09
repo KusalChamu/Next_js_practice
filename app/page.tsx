c17a7d9d0dabeb87ccc28a35412bec0fa5323d56
KusalChamu <chamuchamp09@gmail.com>
2025-12-09 09:50:26 +0530

    Working with Images in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
+          alt="Beautiful Trees"
+          width={500}
+          height={400}
+        />
+
+        <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
