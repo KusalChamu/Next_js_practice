7ba321b31f1fa06341bec7e6399478d359c56cff
KusalChamu <chamuchamp09@gmail.com>
2025-12-09 10:54:04 +0530

    Server vs Client Components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+// states, react hooks belongs to client side so it doesn't work on server side
+
+"use client"
+
+console.log("client or server?")
+
 export default function About() {
     return (
         <div>about</div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,18 @@
-export default function Contact() {
+//server side renders on server and never reach to client. but 1st page or home page loading much faster since it doesn't execute javascript
+//have good SEO
+//can fetch data without craeting api endpoints
+
+import ButtonComponent from "./button";
+
+export default async function Contact() {
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await response.json()
+    console.log(posts);
+
     return (
-        <div>contact</div>
+        <div>
+            <ButtonComponent></ButtonComponent>
+        </div>
     )
 }
